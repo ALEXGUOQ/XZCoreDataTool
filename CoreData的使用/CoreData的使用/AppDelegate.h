2d023217b414dfb1f93ada77994fd4baf5cbea43
Larry <bawangxx@qq.com>
2015-04-02 13:27:32 +0800

    CoreData工具类
    简化Coredata的CRUD操作

---
+++ CoreData的使用/CoreData的使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreData的使用
+//
+//  Created by 李志伟 on 15/3/30.
+//  Copyright (c) 2015年 李志伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreData的使用/CoreData的使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData的使用
+//
+//  Created by 李志伟 on 15/3/30.
+//  Copyright (c) 2015年 李志伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
