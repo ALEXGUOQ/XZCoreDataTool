c7e39bb76e8db300262abc9ae4bebdb73d3f7e5f
Larry <bawangxx@qq.com>
2015-04-08 10:38:17 +0800

    条件查询
    条件查询

--- CoreData的使用/ViewController.m
+++ CoreData的使用/ViewController.m
@@ -34,8 +34,40 @@
 -(void)getEntityWithFetchRequest{
     
     NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    NSPredicate *pre = [NSPredicate predicateWithFormat:@"age like %@",@"21"];
+    
+    //查询年龄以21开头的person
+    NSPredicate *pre = [NSPredicate predicateWithFormat:@"age like %@",@"*21"];
     request.predicate = pre;
+    
+    // 模糊查询
+    // 名字以"wang"开头
+    //    NSPredicate *pre = [NSPredicate predicateWithFormat:@"name BEGINSWITH %@",@"wangwu1"];
+    //    request.predicate = pre;
+    
+    // 名字以"1"结尾
+    //    NSPredicate *pre = [NSPredicate predicateWithFormat:@"name ENDSWITH %@",@"1"];
+    //    request.predicate = pre;
+    
+    
+    // 名字包含"wu1"
+    //    NSPredicate *pre = [NSPredicate predicateWithFormat:@"name CONTAINS %@",@"wu1"];
+    //    request.predicate = pre;
+    
+    
+    
+    //将person进行排序
+    NSSortDescriptor *ageSort = [NSSortDescriptor sortDescriptorWithKey:@"age" ascending:NO];
+    request.sortDescriptors = @[ageSort];
+    
+    //分页查询
+    // 分页的起始索引
+    request.fetchOffset = 0;
+    
+    // 分页的条数
+    request.fetchLimit = 3;
+    
+    
+
     NSArray *persons =[XZCoreDataTool getEntityWithName:@"Person" AndFetchRequest:request];
     
     for (Person *p in persons) {
